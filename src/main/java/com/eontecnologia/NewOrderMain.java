5835ea89ad899a81a31b301300b1b8bac68a5ac8
Evandro Oliveira de Novais <evandro-novais@outlook.com>
2023-10-09 09:59:29 -0300

    Updated logservice and NewOrderMain

--- src/main/java/com/eontecnologia/NewOrderMain.java
+++ src/main/java/com/eontecnologia/NewOrderMain.java
@@ -7,19 +7,21 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
-        var value = "132123,678876,12488";
+        var key = UUID.randomUUID().toString();
+        var value = key + "132125,678878";
         var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
         Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
-            System.out.println("Enviado com sucesso para: " + data.topic() + "::: partition " + data.partition() + " / offset " + data.offset() + "/ timestamp " + data.timestamp());
+            System.out.println("Sucesso enviando : " + data.topic() + "::: partition " + data.partition() + " / offset " + data.offset() + "/ timestamp " + data.timestamp());
         };
         var email = "Thank you! We are processing your order!";
         var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
